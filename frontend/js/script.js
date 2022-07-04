5ae4824c1c3a855d29233c443fa940f12bf6d95d
mr.sokol5253 <mr.sokol5253@gmail.com>
2022-07-05 01:38:40 +0300

    made burger menu and several media queries

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,14 +1,18 @@
-// function showMenu (){
-//     document.querySelector('.submenu').setAttribute("style", "visibility: visible");
-// }
-// function hideMenu() {
-//     document.querySelector('.submenu').setAttribute("style", "visibility: hidden");
-// }
-// window.addEventListener("resize", function() {
-//     if (window.innerWidth >= 1440){
-//         showMenu();
-//     }
-//     else {
-//         hideMenu();
-//     }
-// });
+const awaitTimeout = delay => new Promise(resolve => setTimeout(resolve, delay));
+
+async function showMenu (){
+    document.querySelector('.header__list').style.animation = 'open-menu 0.5s forwards';
+    document.querySelector('.header__close-burger-button').style.display = 'block';
+    document.querySelector('.header__close-burger-button').style.animation = 'appear 0.5s forwards';
+    await awaitTimeout(500).then( () => document.querySelector('.header__close-burger-button').style.display = 'block')
+}
+async function hideMenu() {
+    document.querySelector('.header__list').style.animation = 'close-menu 0.5s forwards';
+    document.querySelector('.header__close-burger-button').style.animation = 'disappear 0.5s forwards';
+    await awaitTimeout(500).then( () => document.querySelector('.header__close-burger-button').style.display = 'none')
+}
+window.addEventListener("resize", function() {
+    if (window.innerWidth < 1450){
+        hideMenu();
+    }
+});
