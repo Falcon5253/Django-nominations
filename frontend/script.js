71c536469736a0523ef3fb5a46015f9fe153828f
mr.sokol5253 <mr.sokol5253@gmail.com>
2022-07-04 13:59:16 +0300

    getting started

---
+++ frontend/script.js
@@ -0,0 +1,14 @@
+// function showMenu (){
+//     document.querySelector('.submenu').setAttribute("style", "visibility: visible");
+// }
+// function hideMenu() {
+//     document.querySelector('.submenu').setAttribute("style", "visibility: hidden");
+// }
+// window.addEventListener("resize", function() {
+//     if (window.innerWidth >= 1440){
+//         showMenu();
+//     }
+//     else {
+//         hideMenu();
+//     }
+// });
