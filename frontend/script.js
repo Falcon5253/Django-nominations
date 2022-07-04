8de25a4b28fb3c4032a831b018632b4c122f800e
mr.sokol5253 <mr.sokol5253@gmail.com>
2022-07-04 13:22:40 +0300

    got outout on page

--- frontend/script.js
+++ frontend/script.js
@@ -3,8 +3,19 @@ let api_ip = "http://127.0.0.1:8000/api/auth/"
 
 fetch(api_ip, { method:'GET'}).then(
     response => {
-        document.body.insertAdjacentHTML('beforeend', response);
-        console.log(response.json())
+        // document.body.insertAdjacentHTML('beforeend', response.json());
+        return response.json()
+    }
+)
+.then (
+    
+    data => {
+        for (let i =0; i < data.length; i++){
+            console.log(data[i]);  
+            document.body.insertAdjacentHTML('beforeend', '<h2>'+data[i]['email']+'</h2');
+            document.body.insertAdjacentHTML('beforeend', '<p>'+data[i]['first_name']+'</p');
+            document.body.insertAdjacentHTML('beforeend', '<p></p>');
+        }
     }
 )
 .catch(
