5cdeb264c7b638b26be85e30b791291378835b4d
mr.sokol5253 <mr.sokol5253@gmail.com>
2022-07-05 21:16:39 +0300

    fixed gunicorn config

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,4 +1,4 @@
 command = '/home/std/django-nominations/backend/.venv/bin/gunicorn'
-pythonpath = '/home/std/backend'
+pythonpath = '/home/std/django-nominations/backend'
 bind="127.0.0.1:8000"
 workers = 3
