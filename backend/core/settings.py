0d7a5adb80de5fdcfbf2ddf80821ba4aab7ef0bd
mr.sokol5253 <mr.sokol5253@gmail.com>
2022-07-10 15:31:57 +0300

    cors check

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -79,13 +79,13 @@ SIMPLE_JWT = {
 
 
 #CORS_ALLOW_ALL_ORIGINS = True
-#CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     #'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
